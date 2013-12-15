bb5e6e00618752deb04f4d733ba126c0fbfa310b
Crowbeak <crowbeak@gmail.com>
2013-12-15 17:24:05 +0900

    R-gun functions

--- js/shapes.js
+++ js/shapes.js
@@ -227,6 +227,74 @@ var rw = params.rowWidth;
     };
     
     
+    var rgun = {
+        //barrel pointing left, etc.
+        left: function (params) {
+            var tiles = params.tileList;
+            var i = params.ii;
+            var ch = params.colHeight;
+            var rw = params.rowWidth;
+            
+            if (((i % rw) < (rw - 2)) && (i >= rw) && (tiles[i].type === tiles[i + 1].type) && (tiles[i].type === tiles[i + 2].type) && (tiles[i].type === tiles[i + 2 - rw].type)) {
+                return true;
+            } else { return false; }
+        },
+        lTiles: function (params) {
+            var i = params.ii;
+            var rw = params.rowWidth;
+            return [i, i + 1, i + 2, i + 2 - rw];
+        },
+        
+        up: function (params) {
+            var tiles = params.tileList;
+            var i = params.ii;
+            var ch = params.colHeight;
+            var rw = params.rowWidth;
+            
+            if (((i % rw) < (rw - 1)) && (i < ((ch - 2) * rw)) && (tiles[i].type === tiles[i + rw].type) && (tiles[i].type === tiles[i + (2 * rw)].type) && (tiles[i].type === tiles[i + (2 * rw) + 1].type)) {
+                return true;
+            } else { return false; }
+        },
+        uTiles: function (params) {
+            var i = params.ii;
+            var rw = params.rowWidth;
+            return [i, i + rw, i + (2 * rw), i + (2 * rw) + 1];
+        },
+        
+        right: function (params) {
+            var tiles = params.tileList;
+            var i = params.ii;
+            var ch = params.colHeight;
+            var rw = params.rowWidth;
+            
+            if (((i % rw) < (rw - 2)) && (i < ((ch - 1) * rw)) && (tiles[i].type === tiles[i + 1].type) && (tiles[i].type === tiles[i + 2].type) && (tiles[i].type === tiles[i + rw].type)) {
+                return true;
+            } else { return false; }
+        },
+        rTiles: function (params) {
+            var i = params.ii;
+            var rw = params.rowWidth;
+            return [i, i + 1, i + 2, i + rw];
+        },
+        
+        down: function (params) {
+            var tiles = params.tileList;
+            var i = params.ii;
+            var ch = params.colHeight;
+            var rw = params.rowWidth;
+            
+            if (((i % rw) < (rw - 1)) && (i < ((ch - 2) * rw)) && (tiles[i].type === tiles[i + 1].type) && (tiles[i].type === tiles[i + 1 + rw].type) && (tiles[i].type === tiles[i + 1 + (2 * rw)].type)) {
+                return true;
+            } else { return false; }
+        },
+        dTiles: function (params) {
+            var i = params.ii;
+            var rw = params.rowWidth;
+            return [i, i + 1, i + 1 + rw, i + 1 + (2 * rw)];
+        }
+    };
+    
+    
     function ReturnObject() {
         this.needToRemove = false;
         this.tilesToRemove = {
@@ -286,6 +354,18 @@ var rw = params.rowWidth;
             returnObj.flagForRemoval(testTile.type);
         } else if (lgun.down(params)) {
             returnObj.tilesToRemove.indices = lgun.dTiles(params);
+            returnObj.flagForRemoval(testTile.type);
+        } else if (rgun.left(params)) {
+            returnObj.tilesToRemove.indices = rgun.lTiles(params);
+            returnObj.flagForRemoval(testTile.type);
+        } else if (rgun.up(params)) {
+            returnObj.tilesToRemove.indices = rgun.uTiles(params);
+            returnObj.flagForRemoval(testTile.type);
+        } else if (rgun.right(params)) {
+            returnObj.tilesToRemove.indices = rgun.rTiles(params);
+            returnObj.flagForRemoval(testTile.type);
+        } else if (rgun.down(params)) {
+            returnObj.tilesToRemove.indices = rgun.dTiles(params);
             returnObj.flagForRemoval(testTile.type);
         }
         
